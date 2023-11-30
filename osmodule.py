a4d550aa46fcf6b5193c37bcbd9621f1bccbfdb3
MOHD RIZWAN <143150040+Rizwansaifi571@users.noreply.github.com>
2023-11-30 22:29:08 +0530

    Add files via upload

---
+++ osmodule.py
@@ -0,0 +1,68 @@
+# import os
+# cwd=os.getcwd()
+# print(f"Current working directory before : {cwd} ")
+# cwd2=os.chdir("C:/Users/Rizwan iitian/OneDrive/Documents/VS CODE PYTHON/code_with_harry")
+# print(f'current working directory after : {cwd2}')
+
+# import os
+# print(os.listdir("C:/Users/Rizwan iitian/OneDrive/Documents/vs code python/code_with_harry"))
+# print(os.getcwd())
+
+# import os
+# print(os.listdir())
+# os.chdir("C:/Users/Rizwan iitian/OneDrive/Documents/vs code python/W3_school")
+# os.mkdir("rose2.py")
+# print(os.listdir())
+
+
+
+# import os
+# os.chdir("C:/Users/Rizwan iitian/OneDrive/Documents/vs code python/W3_school")
+# print(os.listdir())
+# os.makedirs("stream/science_sub/phy,chem,math")
+# print(os.listdir()) 
+
+
+# import os
+# print("before removing ",os.listdir())
+# os.removedirs('stream/science_sub/phy,chem,math')
+# print("after removing ",os.listdir())
+
+# import os
+# print("Before removing ",os.listdir())
+# os.rmdir("rose2.py")
+# print("After removing ",os.listdir())
+
+# import os
+# # os.mkdir("Apple")
+# file_name="Apple"
+# file=os.popen(file_name,'w')
+# file.write("Apples are healthy")
+# file=os.popen(file_name,'r')
+# print(file.read())
+
+# import os
+
+# file_name = "apple.txt"
+# file = os.popen(file_name, 'w')
+# file.write("Apples are healthy")
+# file = os.popen(file_name, 'r')
+# print(file.read())
+
+# import os
+# print("before renaming ",os.listdir())
+# os.renames("Apple","progress")
+# print("After renaming ",os.listdir())
+
+
+# x=10
+# def my_fun():
+#     global x
+#     x=5
+#     global y
+#     y=5
+#     print(y)
+# my_fun()
+# print(x)
+# print(y)
+
